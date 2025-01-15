2b57a313f197021eaae9b1cecef798ee66d38de4
lycilph <lycilph@gmail.com>
2025-01-15 10:39:10 +0100

    Add project files.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Sandbox;
+
+/* Links to stuff:
+ * https://github.com/farhiongit/sudoku-solver
+ * https://github.com/HappyCerberus/sudoku
+ * https://hodoku.sourceforge.net/en/techniques.php
+ * https://norvig.com/sudoku.html
+ * 
+ * Techniques:
+ * https://rakhman.info/blog/solving-sudoku-with-graph-theory/
+ * https://opensourc.es/blog/sudoku/
+ * 
+ * Test data:
+ * https://stackoverflow.com/questions/59973969/high-quality-test-cases-for-sudoku-solver
+ * (info about datasets: https://github.com/t-dillon/tdoku/blob/master/benchmarks/README.md)
+ * 
+ * Todo:
+ * Make a sudoku solver that can solve any sudoku puzzle
+ * Make a sudoku generator that can generate any sudoku puzzle
+ * Make a sudoku validator that can validate any sudoku puzzle
+ * Make a ui in blazor (and hosted on github pages)
+ */
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
