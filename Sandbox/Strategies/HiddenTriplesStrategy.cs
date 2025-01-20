8bb2f406dcf82a46ebfa3ec822ea16e312fe56c8
lycilph <lycilph@gmail.com>
2025-01-20 15:03:32 +0100

    Starting the hidden triples strategy

---
+++ Sandbox/Strategies/HiddenTriplesStrategy.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox.Model;
+
+namespace Sandbox.Strategies;
+
+public class HiddenTriplesStrategy : IStrategy
+{
+    public string Name => "Hidden Triples";
+
+    public static readonly HiddenTriplesStrategy Instance = new();
+
+    public bool Step(Grid grid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool Execute(Grid grid)
+    {
+        return Instance.Step(grid);
+    }
+}
