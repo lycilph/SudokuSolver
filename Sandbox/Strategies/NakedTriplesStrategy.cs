a55d7efa8a589ebc704f52db20dae50d713af333
lycilph <lycilph@gmail.com>
2025-01-20 14:35:57 +0100

    Added naked triples strategy

--- Sandbox/Strategies/NakedTriplesStrategy.cs
+++ Sandbox/Strategies/NakedTriplesStrategy.cs
@@ -23,6 +23,42 @@ public class NakedTriplesStrategy : IStrategy
     {
         var found_triples = false;
 
+        var empty_cells = unit.Cells.Where(c => c.IsEmpty).ToArray();
+        if (empty_cells.Length <= 3)
+            return false;
+
+        var candidates = empty_cells.Where(c => c.CandidatesCount == 2 || c.CandidatesCount == 3).ToArray();
+        if (candidates.Length < 3)
+            return false;
+
+        for (int i = 0; i < candidates.Length - 2; i++)
+        {
+            for (int j = i + 1; j < candidates.Length - 1; j++)
+            {
+                for (int k = j + 1; k < candidates.Length; k++)
+                {
+                    var triple = new Cell[] { candidates[i], candidates[j], candidates[k] };
+                    var triple_candidates = triple.SelectMany(c => c.Candidates).Distinct().ToArray();
+
+                    if (triple_candidates.Length == 3)
+                    {
+                        foreach (var cell in empty_cells)
+                        {
+                            if (cell == candidates[i] || cell == candidates[j] || cell == candidates[k])
+                                continue;
+
+                            if (cell.Candidates.Overlaps(triple_candidates))
+                            {
+                                cell.Candidates.ExceptWith(triple_candidates);
+                                Console.WriteLine($" * Found naked triple ({string.Join(',', triple_candidates)}) in {unit.FullName} removes candidates from {cell.Index}");
+                                found_triples = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         return found_triples;
     }
 
