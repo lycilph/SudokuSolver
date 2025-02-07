01e105e5e11260ca1705d8087b12b48c1f33665c
lycilph <lycilph@gmail.com>
2025-02-07 13:29:20 +0100

    Implemented the dancing links algorithm

---
+++ DancingLinksSolver/ColumnNode.cs
@@ -0,0 +1,14 @@
+﻿namespace DancingLinksSolver;
+
+public class ColumnNode : DLXNode
+{
+    public int Size; // Number of 1s in the column
+    public string Name;
+
+    public ColumnNode(string name)
+    {
+        Name = name;
+        Size = 0;
+        Column = this;
+    }
+}
---
+++ DancingLinksSolver/DLXNode.cs
@@ -0,0 +1,10 @@
+﻿namespace DancingLinksSolver;
+
+public class DLXNode
+{
+    public DLXNode Left, Right, Up, Down;
+    public ColumnNode Column = null!;
+    public int RowID = -1;
+
+    public DLXNode() { Left = Right = Up = Down = this; }
+}
