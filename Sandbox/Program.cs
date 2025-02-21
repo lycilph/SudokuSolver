280f3a67e4020baf84330c83fde1638d3f95d209
lycilph <lycilph@gmail.com>
2025-02-21 12:32:13 +0100

    Added the dancing links solver back in

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Algorithms;
+using Core.Archive.DancingLinks;
 using Core.Model;
 
 namespace Sandbox;
@@ -7,8 +8,27 @@ internal class Program
 {
     static void Main()
     {
-        var p = new Puzzle("4.....938.32.941...953..24.37.6.9..4529..16736.47.3.9.957..83....39..4..24..3.7.9"); 
-            
+        //var p = new Puzzle("4......38.32.941...953..24.37.6.9..4.29..16.36.47.3.9.957..83....39..4..24..3.7.9"); // 1 solution
+        var p = new Puzzle("4......3..32..41...953..24.37.6.9..4.29..16.36.47.3.9...7..83....39..4..24....7.9"); // 24 solutions
+
+        var results = DancingLinksSolver.Solve(p);
+
+        Console.WriteLine($"{results.Count} solutions found");
+        Console.WriteLine($"Execution Time: {p.Stats.ElapsedTime} ms");
+        Console.WriteLine();
+
+        Console.WriteLine("Solution(s):");
+        foreach (var solution in results)
+            Console.WriteLine(solution.ToSimpleString());
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    private static void TestStrategySolver()
+    {
+        var p = new Puzzle("4.....938.32.941...953..24.37.6.9..4529..16736.47.3.9.957..83....39..4..24..3.7.9");
+
         Console.WriteLine(p.Grid.ToString());
         Solver.Solve(p);
 
@@ -23,12 +43,10 @@ internal class Program
         {
             Console.WriteLine("Solved state");
             Console.WriteLine(p.Grid.ToString());
+            Console.WriteLine(p.Grid.ToSimpleString());
             Console.WriteLine();
         }
         else
             Console.WriteLine("NO SOLUTION FOUND");
-
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
     }
 }
