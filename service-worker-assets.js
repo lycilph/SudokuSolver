443669985f198c1e6dccd285bcbafa81f0d46f03
lycilph <lycilph@gmail.com>
2025-03-21 20:09:58 +0000

    Deploying to gh-pages from @ lycilph/SudokuSolver@769ac828f06e6c2174aaf85fb70fc2da7dec9b89 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "eY8nxj8J",
+  "version": "eHOhqps/",
   "assets": [
     {
       "hash": "sha256-zxO7dbsyE5vVDT6gjugl6weEl3Y4pkNIPisIe+QkFG0=",
@@ -62,8 +62,8 @@ self.assetsManifest = {
       "url": "_framework/Microsoft.JSInterop.ceii4uvike.wasm"
     },
     {
-      "hash": "sha256-JXntMzyRnY7sNTa+BSEbT+oA2ncYloIqfOUexCCE4m0=",
-      "url": "_framework/SudokuBlazorUI.fzy7e7dvq1.wasm"
+      "hash": "sha256-/R3gQ9/kGUdvUxzNkzH9En9hdtjzXaOgqcSPEZc3iaw=",
+      "url": "_framework/SudokuBlazorUI.pxonk7730k.wasm"
     },
     {
       "hash": "sha256-dV96G/HF1YeoQHWb7qAz79rlxBOm2/Tq24EUOLmfEew=",
@@ -142,7 +142,7 @@ self.assetsManifest = {
       "url": "_framework/System.Text.RegularExpressions.k97bmwwlww.wasm"
     },
     {
-      "hash": "sha256-SarRhqrb1mz50CoGMiInh80hfDDfP3eaOocVSS6tVjg=",
+      "hash": "sha256-GZYFTmxl+OlYAyMu0WOwtZ6yZmg04EKYtXUg4o7y1m0=",
       "url": "_framework/blazor.boot.json"
     },
     {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: eY8nxj8J */
+/* Manifest version: eHOhqps/ */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
