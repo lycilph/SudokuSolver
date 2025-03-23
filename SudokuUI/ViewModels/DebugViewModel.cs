0bdd889a3d475853b9e68b562611d85272877341
lycilph <lycilph@gmail.com>
2025-03-23 09:34:48 +0100

    Stuff

--- SudokuUI/ViewModels/DebugViewModel.cs
+++ SudokuUI/ViewModels/DebugViewModel.cs
@@ -1,13 +1,17 @@
-﻿using Core.Commands;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Core.Commands;
 using ObservableCollections;
 using SudokuUI.Services;
 
 namespace SudokuUI.ViewModels;
 
-public class DebugViewModel
+public partial class DebugViewModel : ObservableObject
 {
     public NotifyCollectionChangedSynchronizedViewList<ICommand> UndoStack { get; private set; }
     public NotifyCollectionChangedSynchronizedViewList<ICommand> RedoStack { get; private set; }
+
+    [ObservableProperty]
+    private ICommand? selected;
 
     public DebugViewModel(UndoRedoService undo_service)
     {
