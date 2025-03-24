107c9e8637ded54adc703f399138904b3cf3859d
lycilph <lycilph@gmail.com>
2025-03-24 15:57:52 +0100

    Adding strategies back

--- SudokuUI/ViewModels/DebugViewModel.cs
+++ SudokuUI/ViewModels/DebugViewModel.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Core.Commands;
 using Core.Extensions;
-using Core.Strategies;
 using ObservableCollections;
 using SudokuUI.Services;
 
@@ -24,5 +24,14 @@ public partial class DebugViewModel : ObservableObject
         RedoStack = undo_service.RedoStack.ToNotifyCollectionChanged();
 
         Strategies = solver_service.Strategies.Select(s => new StrategyViewModel(s)).ToObservableCollection();
+    }
+
+    [RelayCommand]
+    private void Toggle()
+    {
+        if (Strategies.All(s => s.Selected))
+            Strategies.ForEach(s => s.Selected = false);
+        else
+            Strategies.ForEach(s => s.Selected = true); 
     }
 }
--- SudokuUI/ViewModels/StrategyViewModel.cs
+++ SudokuUI/ViewModels/StrategyViewModel.cs
@@ -1,15 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Core.Commands;
 using Core.Strategies;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+using SudokuUI.Services;
 
 namespace SudokuUI.ViewModels;
 
 public partial class StrategyViewModel : ObservableObject
 {
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    private readonly PuzzleService puzzle_service;
+
     [ObservableProperty]
     private IStrategy wrappedObject;
 
+    [ObservableProperty]
+    private bool selected = false;
+
     public StrategyViewModel(IStrategy strategy)
     {
+        puzzle_service = App.Current.Services.GetRequiredService<PuzzleService>();
+
         WrappedObject = strategy;
+    }
+
+    [RelayCommand]
+    private void Execute()
+    {
+        logger.Info($"Executing strategy {WrappedObject.Name}");
+
+        if (WrappedObject.Plan(puzzle_service.Grid) is BaseCommand command)
+        {
+            logger.Info($"{command.Name} is {command.IsValid()}");
+            command.Do();
+        }
+        else
+            logger.Info("Strategy invalid");
     }
 }
