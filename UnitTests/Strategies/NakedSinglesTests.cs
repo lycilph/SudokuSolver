a4b1e7791aed722484e4517f97ed39c55bebab9d
lycilph <lycilph@gmail.com>
2025-03-25 10:43:01 +0100

    Unit test

---
+++ UnitTests/Strategies/NakedSinglesTests.cs
@@ -0,0 +1,27 @@
+﻿using Core.Commands;
+using Core.Extensions;
+using Core.Models;
+using Core.Strategies;
+
+namespace UnitTests.Strategies;
+
+// See more information in the NakedSinglesStrategy class
+public class NakedSinglesTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        // This is a simple puzzle, solvable by only basic eliminations and naked singles
+        var input = ".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4";
+        var grid = new Grid().Load(input, true);
+        BasicEliminationStrategy.PlanAndExecute(grid);
+
+        // Act
+        var result = NakedSinglesStrategy.PlanAndExecute(grid) as BaseCommand;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(10, result.Elements.Count);
+    }
+}
