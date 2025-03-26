e5f2db791dab7f8a8f6bbc6d4f93222d0a5f9277
lycilph <lycilph@gmail.com>
2025-03-26 08:19:41 +0100

    Added a list of predefined puzzles to the debug window to ease debugging

---
+++ SudokuUI/Infrastructure/ExpanderIndexConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SudokuUI.Infrastructure;
+
+public class ExpanderIndexConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        int active_index = (int)value;
+        int this_index = int.Parse(parameter.ToString() ?? "-1");
+        return active_index == this_index;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool is_expanded = (bool)value;
+        int this_index = int.Parse(parameter.ToString() ?? "-1");
+        return is_expanded ? this_index : -1; // -1 means none expanded
+    }
+}
--- SudokuUI/ViewModels/DebugViewModel.cs
+++ SudokuUI/ViewModels/DebugViewModel.cs
@@ -24,6 +24,24 @@ public partial class DebugViewModel : ObservableObject
     [ObservableProperty]
     private ICommand? selected;
 
+    [ObservableProperty]
+    private int activeExpander = 0;
+
+    [ObservableProperty]
+    private Tuple<string, string> selectedPuzzle;
+
+    [ObservableProperty]
+    private ObservableCollection<Tuple<string, string>> puzzles = 
+        [
+            Tuple.Create("Naked Singles", ".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4"),
+            Tuple.Create("Hidden Singles", ".28..7....16.83.7.....2.85113729.......73........463.729..7.......86.14....3..7.."),
+            Tuple.Create("Pointing Candidates 1", ".179.36......8....9.....5.7.72.1.43....4.2.7..6437.25.7.1....65....3......56.172."),
+            Tuple.Create("Pointing Candidates 2", "93..5....2..63..95856..2.....318.57...5.2.98..8...5......8..1595.821...4...56...8"),
+            Tuple.Create("Claiming Candidates 1", ".16..78.3.9.8.....87...126..48...3..65...9.82.39...65..6.9...2..8...29369246..51."),
+            Tuple.Create("Claiming Candidates 2", ".2.9437159.4...6..75.....4.5..48....2.....4534..352....42....81..5..426..9.2.85.4"),
+            Tuple.Create("Naked Pairs", "4.....938.32.941...953..24.37.6.9..4529..16736.47.3.9.957..83....39..4..24..3.7.9")
+        ];
+
     public DebugViewModel(PuzzleService puzzle_service, UndoRedoService undo_service, SolverService solver_service)
     {
         this.puzzle_service = puzzle_service;
@@ -33,6 +51,8 @@ public partial class DebugViewModel : ObservableObject
         RedoStack = undo_service.RedoStack.ToNotifyCollectionChanged();
 
         Strategies = solver_service.Strategies.Select(s => new StrategyViewModel(s)).ToObservableCollection();
+
+        SelectedPuzzle = Puzzles.First();
     }
 
     [RelayCommand]
@@ -54,5 +74,11 @@ public partial class DebugViewModel : ObservableObject
             undo_service.Execute(command);
         else
             WeakReferenceMessenger.Default.Send(new ShowNotificationMessage("No more hints found"));
+    }
+
+    [RelayCommand]
+    private void Load()
+    {
+        puzzle_service.Import(SelectedPuzzle.Item2);
     }
 }
