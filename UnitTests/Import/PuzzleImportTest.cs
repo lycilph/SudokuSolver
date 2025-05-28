be9469f8b62e9837b54977ef19a2509a9259a2d0
lycilph <lycilph@gmail.com>
2025-05-28 12:26:50 +0200

    Updated nuget packages

---
+++ UnitTests/Import/PuzzleImportTest.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Core.Import;
+using OpenCvSharp;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UnitTests.Import;
+
+public class PuzzleImportTest
+{
+    [Fact]
+    public void ImportPuzzle()
+    {
+        var path = @"C:\Users\Morten Lang\source\repos\SudokuSolver\Data\Importer\";
+        var image_filename = "IMG_20250330_101246.jpg";
+        var puzzle_filename = Path.ChangeExtension(image_filename, "txt");
+
+        var importer = new PuzzleImporter();
+        var config = ImportConfiguration.Default();
+
+        using (var t = new ResourcesTracker())
+        {
+            var image = t.T(new Mat(Path.Combine(path, image_filename)));
+            var imported_puzzle = importer.Import(image, config);
+            var actual_puzzle = File.ReadAllText(Path.Combine(path, puzzle_filename));
+
+            Assert.Equal(imported_puzzle, actual_puzzle);
+        }
+    }
+}
